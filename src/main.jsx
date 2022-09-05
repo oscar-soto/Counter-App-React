2b0385a6b4e344bf876e9747d24ca30e1141b705
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-05 14:04:10 -0400

    hello word

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Hola Mundo!!</h1>;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
