a98905b31b5eb3e1e5410f83c7e841f7b978cb1d
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-05 23:53:32 -0400

    Fix: learn about hooks: useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { FirstApp } from "./FirstApp";
-import { HelloWorldApp } from "./HelloWorldApp";
+import { CounterApp } from "./CounterApp";
+// import { FirstApp } from "./FirstApp";
+// import { HelloWorldApp } from "./HelloWorldApp";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelloWorldApp />
-    <FirstApp />
+    {/* <HelloWorldApp /> */}
+    {/* <FirstApp /> */}
+    <CounterApp value={4} />
   </React.StrictMode>
 );
